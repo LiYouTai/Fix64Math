666b7ea52cff750922c628620796d5938b570f73
likai <1147159782@qq.com>
2024-02-25 13:36:01 +0800

    0

---
+++ Math/Fix64Vector2.cs
@@ -0,0 +1,145 @@
+﻿using UnityEngine;
+namespace LookStep.VGLookStep.Math
+{
+    public struct Fix64Vector2
+    {
+
+        public static Fix64Vector2 Up = new Fix64Vector2(0, 1);
+        public static Fix64Vector2 Zero = new Fix64Vector2(0, 0);
+
+        public Fix64 X;
+        public Fix64 Y;
+
+        public static Fix64Vector2 operator /(Fix64Vector2 a, Fix64 cs)
+        {
+            return new Fix64Vector2(a.X / cs, a.Y / cs);
+        }
+
+        public static Fix64Vector2 operator *(Fix64Vector2 a, Fix64 cs)
+        {
+            return new Fix64Vector2(a.X * cs, a.Y * cs);
+        }
+
+
+        public static Fix64Vector2 operator *(Fix64Vector2 a, int cs)
+        {
+            return new Fix64Vector2(a.X * (Fix64)cs, a.Y * (Fix64)cs);
+        }
+
+        public static Fix64Vector2 operator -(Fix64Vector2 a, Fix64Vector2 cs)
+        {
+            return new Fix64Vector2(a.X - cs.X, a.Y - cs.Y);
+        }
+
+        public static Fix64Vector2 operator -(Fix64Vector2 a)
+        {
+            return new Fix64Vector2(-a.X, -a.Y);
+        }
+
+        public static Fix64Vector2 operator +(Fix64Vector2 a, Fix64Vector2 cs)
+        {
+            return new Fix64Vector2(a.X + cs.X, a.Y + cs.Y);
+        }
+
+        public static bool operator ==(Fix64Vector2 x, Fix64Vector2 y)
+        {
+            return x.X == y.X && x.Y == y.Y;
+        }
+        public static bool operator !=(Fix64Vector2 x, Fix64Vector2 y)
+        {
+            return x.X != y.X || x.Y != y.Y;
+        }
+        public Fix64Vector2(int x, int y)
+        {
+            X = new Fix64(x);
+            Y = new Fix64(y);
+        }
+
+        public Fix64Vector2(float x, float y)
+        {
+            X = new Fix64(x);
+            Y = new Fix64(y);
+        }
+
+        public Fix64Vector2(Fix64 x, Fix64 y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public Fix64Vector2(Vector2 v)
+        {
+            X = new Fix64(v.x);
+            Y = new Fix64(v.y);
+        }
+        public Fix64 Magnitude()
+        {
+            return Fix64.Sqrt((X * X) + (Y * Y));
+        }
+
+        public Fix64Vector2 Nomalize()
+        {
+            return this / this.Magnitude();
+        }
+
+        public Vector2 ToVector2()
+        {
+            return new Vector2((float)X, (float)Y);
+        }
+
+        public Vector3 ToVector3()
+        {
+            return new Vector3((float)X, (float)Y, 0);
+        }
+
+        public Vector3 ToVector3Zero()
+        {
+            return new Vector3((float)X, (float)Y, 0);
+        }
+
+        public static Fix64Vector2 Lerp(Fix64Vector2 a, Fix64Vector2 b, Fix64 t)
+        {
+            return a + (b - a) * t;
+        }
+
+        public static Fix64 Distance(Fix64Vector2 a, Fix64Vector2 b)
+        {
+            Fix64 a2 = Fix64.FastAbs(a.X - b.X);
+            Fix64 b2 = Fix64.FastAbs(a.Y - b.Y);
+
+            return Fix64.Sqrt(a2 * a2 + b2 * b2);
+        }
+
+        public static Fix64 Dot(Fix64Vector2 a, Fix64Vector2 b)
+        {
+            return (a.X * b.X) + (a.Y * b.Y);
+        }
+
+        public static Fix64 Angle(Fix64Vector2 a, Fix64Vector2 b)
+        {
+            return Fix64.Acos(Dot(a.Nomalize(), b.Nomalize()));
+        }
+
+        public static Fix64Vector2 Rotate(Fix64Vector2 iA, Fix64 degrees)
+        {
+            degrees *= Fix64.DegreeToRad;
+            Fix64Vector2 A = new Fix64Vector2(0, 0);
+            A.X = (iA.X * Fix64.Cos(degrees) - iA.Y * Fix64.Sin(degrees));
+            A.Y = (iA.X * Fix64.Sin(degrees) + iA.Y * Fix64.Cos(degrees));
+
+            return A;
+        }
+
+        public static Fix64Vector2 SetVertex(Fix64 X, Fix64 Y)
+        {
+            return new Fix64Vector2(X, Y);
+        }
+
+        public override string ToString()
+        {
+            return "X:" + X.ToString() + "Y:" + Y.ToString();
+        }
+
+
+    }
+}
---
+++ Math/Fix64Vector3.cs
@@ -0,0 +1,165 @@
+﻿using UnityEngine;
+namespace LookStep.VGLookStep.Math
+{
+    public struct Fix64Vector3
+    {
+
+        public static Fix64Vector3 Up = new Fix64Vector3(0, 1, 0);
+        public static Fix64Vector3 One = new Fix64Vector3(1, 1, 1);
+        public static Fix64Vector3 Zero = new Fix64Vector3(0, 0, 0);
+
+        public Fix64 X;
+        public Fix64 Y;
+        public Fix64 Z;
+
+        public static Fix64Vector3 operator /(Fix64Vector3 a, Fix64 cs)
+        {
+            return new Fix64Vector3(a.X / cs, a.Y / cs, a.Z / cs);
+        }
+
+        public static Fix64Vector3 operator /(Fix64 cs, Fix64Vector3 a)
+        {
+            return new Fix64Vector3(cs / a.X, cs / a.Y, cs / a.Z);
+        }
+
+        public static Fix64Vector3 operator *(Fix64Vector3 a, Fix64 cs)
+        {
+            return new Fix64Vector3(a.X * cs, a.Y * cs, a.Z * cs);
+        }
+
+
+        public static Fix64Vector3 operator *(Fix64Vector3 a, int cs)
+        {
+            return new Fix64Vector3(a.X * (Fix64)cs, a.Y * (Fix64)cs, a.Z * (Fix64)cs);
+        }
+
+        public static Fix64Vector3 operator *(Fix64Vector3 a, Fix64Vector3 b)
+        {
+            return new Fix64Vector3(a.X * b.X, a.Y * b.Y, a.Z * b.Z);
+        }
+
+
+        public static Fix64Vector3 operator -(Fix64Vector3 a, Fix64Vector3 cs)
+        {
+            return new Fix64Vector3(a.X - cs.X, a.Y - cs.Y, a.Z - cs.Z);
+        }
+
+        public static Fix64Vector3 operator -(Fix64Vector3 a)
+        {
+            return new Fix64Vector3(-a.X, -a.Y, -a.Z);
+        }
+
+        public static Fix64Vector3 operator +(Fix64Vector3 a, Fix64Vector3 cs)
+        {
+            return new Fix64Vector3(a.X + cs.X, a.Y + cs.Y, a.Z + cs.Z);
+        }
+
+        public static bool operator ==(Fix64Vector3 a, Fix64Vector3 b)
+        {
+            return a.X == b.X && a.Y == b.Y && a.Z == b.Z;
+        }
+        public static bool operator !=(Fix64Vector3 a, Fix64Vector3 b)
+        {
+            return a.X != b.X || a.Y != b.Y || a.Z != b.Z;
+        }
+        public Fix64Vector3(int x, int y, int z)
+        {
+            X = new Fix64(x);
+            Y = new Fix64(y);
+            Z = new Fix64(z);
+        }
+
+        public Fix64Vector3(float x, float y, float z)
+        {
+            X = new Fix64(x);
+            Y = new Fix64(y);
+            Z = new Fix64(z);
+        }
+
+        public Fix64Vector3(Fix64 x, Fix64 y, Fix64 z)
+        {
+            X = x;
+            Y = y;
+            Z = z;
+        }
+        public Fix64Vector3(Vector3 v)
+        {
+            X = new Fix64(v.x);
+            Y = new Fix64(v.y);
+            Z = new Fix64(v.z);
+        }
+
+        public Fix64 Magnitude()
+        {
+            return Fix64.Sqrt((X * X) + (Y * Y) + (Z * Z));
+        }
+
+        public Fix64Vector3 Nomalize()
+        {
+            return this / this.Magnitude();
+        }
+
+        public Vector2 ToVector2()
+        {
+            return new Vector2((float)X, (float)Y);
+        }
+
+        public Vector3 ToVector3()
+        {
+            return new Vector3((float)X, (float)Y, (float)Z);
+        }
+
+        public Fix64Vector2 ToFix64Vector2()
+        {
+            return new Fix64Vector2(X, Y);
+        }
+        public static Fix64Vector3 Lerp(Fix64Vector3 a, Fix64Vector3 b, Fix64 t)
+        {
+            return a + (b - a) * t;
+        }
+
+        public static Fix64 Distance(Fix64Vector3 a, Fix64Vector3 b)
+        {
+            Fix64 xx = Fix64.FastAbs(a.X - b.X);
+            Fix64 yy = Fix64.FastAbs(a.Y - b.Y);
+            Fix64 zz = Fix64.FastAbs(a.Z - b.Z);
+            return Fix64.Sqrt(xx * xx + yy * yy + zz * zz);
+        }
+
+        public static Fix64 Dot(Fix64Vector3 a, Fix64Vector3 b)
+        {
+            return (a.X * b.X) + (a.Y * b.Y) + (a.Z * b.Z);
+        }
+        //public static Fix64Vector3 Dot(Fix64Vector3 a, Fix64Vector3 b)
+        //{
+        //    return new Fix64Vector3(a.X * b.X, a.Y * b.Y, a.Z * b.Z);
+        //}
+
+        public static Fix64Vector3 SetVertex(Fix64 X, Fix64 Y, Fix64 Z)
+        {
+            return new Fix64Vector3(X, Y, Z);
+        }
+
+        public override string ToString()
+        {
+            return "X:" + X.ToString() + "Y:" + Y.ToString() + "Z:" + Z.ToString();
+        }
+
+        public static Fix64Vector3 Max(Fix64Vector3 a, Fix64Vector3 b)
+        {
+            Fix64Vector3 max = a;
+            max.X = Fix64.Max(max.X, b.X);
+            max.Y = Fix64.Max(max.Y, b.Y);
+            max.Z = Fix64.Max(max.Z, b.Z);
+            return max;
+        }
+        public static Fix64Vector3 Min(Fix64Vector3 a, Fix64Vector3 b)
+        {
+            Fix64Vector3 min = a;
+            min.X = Fix64.Min(min.X, b.X);
+            min.Y = Fix64.Min(min.Y, b.Y);
+            min.Z = Fix64.Min(min.Z, b.Z);
+            return min;
+        }
+    }
+}
